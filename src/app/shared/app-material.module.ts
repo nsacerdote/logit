5331b5fc032202fe3ad1e980ec7b6749a5dcead9
Natalio Sacerdote <nsacerdote@users.noreply.github.com>
2019-07-19 22:29:20 +0100

    Upgrade Angular Material to version 8

--- src/app/shared/app-material.module.ts
+++ src/app/shared/app-material.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
-import {
-   MatButtonModule,
-   MatCardModule, MatDatepickerModule,
-   MatDividerModule,
-   MatFormFieldModule,
-   MatIconModule,
-   MatInputModule,
-   MatProgressBarModule,
-   MatSidenavModule,
-   MatToolbarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
