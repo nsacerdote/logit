84421c7cbff7052cd4d4f793c5ed5b01038aa38b
Natalio Sacerdote <nsacerdote@users.noreply.github.com>
2021-06-22 00:19:31 +0100

    Small refactoring and add login check

--- src/app/services/you-track.service.ts
+++ src/app/services/you-track.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { combineLatest, from, Observable, of } from 'rxjs';
+import { combineLatest, concat, from, Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Worklog } from '../models/worklog.model';
 import { Issue } from '../models/issue.model';
 import { UserInfo } from '../models/user-info.model';
 import { Server } from './server.interface';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { ElectronService } from './electron.service';
 import { SettingsService } from './settings.service';
 
@@ -17,6 +17,20 @@ import { SettingsService } from './settings.service';
    providedIn: 'root'
 })
 export class YouTrackService implements Server {
+   constructor(
+      electronService: ElectronService,
+      private settingsService: SettingsService
+   ) {
+      this.axios = electronService.remote.require('axios');
+      this.settingsService
+         .getJiraUrl()
+         .pipe(tap(jiraUrl => (this.serverUrl = `${jiraUrl}`)))
+         .subscribe();
+   }
+
+   private get apiUrl() {
+      return `${this.serverUrl}/youtrack/api`;
+   }
    private axios: typeof axios;
    private serverUrl: string;
    private credentials: {
@@ -34,63 +48,70 @@ export class YouTrackService implements Server {
       };
    }
 
-   constructor(
-      electronService: ElectronService,
-      private settingsService: SettingsService
-   ) {
-      this.axios = electronService.remote.require('axios');
-      this.settingsService
-         .getJiraUrl()
-         .pipe(tap(jiraUrl => (this.serverUrl = `${jiraUrl}`)))
-         .subscribe();
+   private static toIssue(r: { idReadable; summary }) {
+      return new Issue(r.idReadable, r.summary);
    }
 
    sendWorkLogs(workLogs: Worklog[]): Observable<Worklog[]> {
-      return combineLatest(workLogs.map(w => this.sendWorklog(w)));
+      return combineLatest(workLogs.map(w => this.sendWorkLog(w)));
    }
 
-   sendWorklog(worklog: Worklog): Observable<Worklog> {
-      if (worklog.isSent()) {
-         return of(worklog);
+   private sendWorkLog(workLog: Worklog): Observable<Worklog> {
+      if (workLog.isSent()) {
+         return of(workLog);
       }
-      worklog.setAsSending();
+
+      return concat(
+         setAsSending(),
+         this.apiSendWorklog(workLog).pipe(
+            map(result => setAsSent(result.data)),
+            catchError(err => setAsError(err))
+         )
+      );
+
+      function setAsSending() {
+         workLog.setAsSending();
+         return of(workLog);
+      }
+
+      function setAsSent(result) {
+         workLog.setAsSent(result.id);
+         return workLog;
+      }
+
+      function setAsError(err: any) {
+         console.error(err);
+         workLog.setAsError('whatever!');
+         return of(workLog);
+      }
+   }
+
+   private apiSendWorklog(worklog: Worklog) {
       return from(
          this.axios.post(
             `${this.apiUrl}/issues/${worklog.issue.key}/timeTracking/workItems`,
             YouTrackService.buildWorkLogBody(worklog),
             this.authHeaders()
          )
-      ).pipe(
-         map(result => setAsSent(result)),
-         catchError(err => setAsError(err))
-      );
-
-      function setAsSent(result: AxiosResponse<any>) {
-         worklog.setAsSent(result.data.id);
-         return worklog;
-      }
-
-      function setAsError(err: any) {
-         console.error(err);
-         worklog.setAsError('whatever!');
-         return of(worklog);
-      }
+      ).pipe(map(r => r.data));
    }
 
    searchIssues(text: string): Observable<Issue[]> {
-      return from(
-         this.axios.get(`${this.apiUrl}/issues`, {
-            ...this.authHeaders(),
-            params: { query: text, fields: 'idReadable,summary' }
-         })
-      ).pipe(
-         map(response =>
-            response.data.map(
-               (youtrackIssue: any) =>
-                  new Issue(youtrackIssue.idReadable, youtrackIssue.summary)
-            )
-         )
+      return this.apiSearch(text).pipe(
+         map(results => results.map(r => YouTrackService.toIssue(r)))
       );
+   }
+
+   private apiSearch(text: string): Observable<Array<{ idReadable; summary }>> {
+      return from(
+         this.axios.get<Array<{ idReadable; summary }>>(
+            `${this.apiUrl}/issues`,
+            {
+               ...this.authHeaders(),
+               params: { query: text, fields: 'idReadable,summary' }
+            }
+         )
+      ).pipe(map(r => r.data));
    }
 
    deleteWorkLog(workLog: Worklog) {
@@ -103,18 +124,38 @@ export class YouTrackService implements Server {
    }
 
    checkAndSaveCredentials(username, password): Observable<UserInfo> {
+      return this.credentialsCheck(username, password).pipe(
+         tap(response => this.checkResponse(response)),
+         tap(() => this.saveCredentials(username, password)),
+         map(result => this.toUserInfo(result))
+      );
+   }
+
+   private credentialsCheck(username, password): Observable<YoutrackUserInfo> {
       return from(
          this.axios.get<YoutrackUserInfo>(`${this.apiUrl}/users/me`, {
             ...this.authHeaders({ username, password }),
-            params: { fields: 'name,avatarUrl' }
+            params: { fields: 'name,avatarUrl,guest' }
          })
-      ).pipe(
-         tap(() => (this.credentials = { username, password })),
-         map(response => ({
-            displayName: response.data.name,
-            avatarUrl: response.data.avatarUrl
-         }))
-      );
+      ).pipe(map(r => r.data));
+   }
+
+   private checkResponse(response: YoutrackUserInfo) {
+      if (response.guest) {
+         throw { response : { status : 401 } };
+      }
+   }
+
+
+   private saveCredentials(username, password) {
+      this.credentials = { username, password };
+   }
+
+   private toUserInfo(input: YoutrackUserInfo): UserInfo {
+      return {
+         displayName: input.name,
+         avatarUrl: input.avatarUrl
+      };
    }
 
    clearCredentials() {
@@ -129,12 +170,10 @@ export class YouTrackService implements Server {
       return { auth: credentials || this.credentials };
    }
 
-   private get apiUrl() {
-      return `${this.serverUrl}/youtrack/api`;
-   }
 }
 
 interface YoutrackUserInfo {
    name: string;
    avatarUrl: string;
+   guest: boolean;
 }
